5249e0374834b4db2fbdeef665cf9f39549300b1
Andrey Shulga <solyariz@gmail.com>
2016-09-29 17:11:34 +0600

    Ful new version of program. Added class Martix for arrays' type. Class Matrix: added  method getArray.

--- src/main/java/com/epam/as/matrix/Matrix.java
+++ src/main/java/com/epam/as/matrix/Matrix.java
@@ -1,43 +1,25 @@
 package com.epam.as.matrix;
 
 /**
- * Object with arrays' fields.
+ * Create symetric array
  */
 public class Matrix {
     public int[][] array;
-    public int[][] array1;
-    public int[][] array2;
 
     /**
-     * @param array symmetric array
+     * @param rows rows of array
+     * @param cols columns of array
      */
-    public Matrix(int[][] array) {
-        this.array = array;
+    public Matrix(int rows, int cols) {
+        this.array = new int[rows][cols];
     }
 
     /**
-     * @param array1 symmetric array
-     * @param array2 symmetric array
+     * @param rows index of rows of array
+     * @param cols index columns of array
+     * @return element of array
      */
-    public Matrix(int array1[][], int array2[][]) {
-        this.array1 = array1;
-        this.array2 = array2;
-    }
-
-    /**
-     * Multiplication matrices.
-     *
-     * @param array1 symmetric array
-     * @param array2 symmetric array
-     * @return filled array with values which are result of multiplication two arrays.
-     */
-    public int[][] multiplication(int[][] array1, int[][] array2) {
-        int[][] resArray = new int[array1.length][array1.length];
-        for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array2.length; j++) {
-                resArray[i][j] = array1[i][j] * array2[i][j];
-            }
-        }
-        return resArray;
+    public int getArray(int rows, int cols) {
+        return array[rows][cols];
     }
 }
--- src/main/java/com/epam/as/matrix/MatrixTest.java
+++ src/main/java/com/epam/as/matrix/MatrixTest.java
@@ -16,54 +16,13 @@ public class MatrixTest {
     public static void main(String[] args) throws IOException {
 
         int arrSize;
-        int[][] result;
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter size of symmetric matrices");
         arrSize = parseInt(reader.readLine());
 
-        //constructing a object of type Matrix with arrays' parameters
-        Matrix matrix = new Matrix(fillArray(arrSize), fillArray(arrSize));
 
-        System.out.println("Array#1");
-        printArray(matrix.array1);
-        System.out.println("Array#2");
-        printArray(matrix.array2);
-
-        //multiply two arrays on each other
-        result = matrix.multiplication(matrix.array1, matrix.array2);
-
-        System.out.println("Result of multiplication Array#1 with Array#2");
-        printArray(result);
     }
 
-    /**
-     * Fill array[][] with random number from 0 to 9.
-     *
-     * @param arrSize size of array
-     * @return filled array[][]
-     */
-    private static int[][] fillArray(int arrSize) {
-        int[][] mas = new int[arrSize][arrSize];
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 0; j < mas.length; j++) {
-                mas[i][j] = (int) (Math.random() * 10);
-            }
-        }
-        return mas;
-    }
 
-    /**
-     * Output array to console.
-     *
-     * @param array [][]
-     */
-    public static void printArray(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.print(array[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
 }
